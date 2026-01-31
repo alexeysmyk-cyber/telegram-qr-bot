5d9c05fca89073d0eb4475eeb5cdee4e7a220248
alexeysmyk-cyber <alexey.smyk@gmail.com>
2026-01-31 15:46:53 +0400

    Update app.js

--- miniapp/app.js
+++ miniapp/app.js
@@ -16,7 +16,7 @@ const content = document.getElementById('content');
 const visitsTab = document.getElementById('visitsTab');
 const scheduleTab = document.getElementById('scheduleTab');
 
-//import { renderCalendar } from './calendar.js';
+import { renderCalendar } from './calendar.js';
 
 // ===============================
 // Авторизация через backend
@@ -122,88 +122,6 @@ renderCalendar(calendarEl, (date) => {
 }
 
 
-function renderCalendar(container, onSelect) {
-
-  let current = new Date();
-  current.setDate(1);
-
-  const today = new Date();
-  today.setHours(0,0,0,0);
-
-  let touchStartX = 0;
-  let touchEndX = 0;
-
-  function build(year, month) {
-    container.innerHTML = "";
-
-    const header = document.createElement("div");
-    header.className = "cal-header";
-
-    const title = document.createElement("div");
-    title.className = "cal-title";
-    title.innerText =
-      new Date(year, month)
-        .toLocaleString("ru-RU", { month: "long", year: "numeric" });
-
-    header.appendChild(title);
-    container.appendChild(header);
-
-    const grid = document.createElement("div");
-    grid.className = "cal-grid";
-
-    const daysOfWeek = ["Пн","Вт","Ср","Чт","Пт","Сб","Вс"];
-    daysOfWeek.forEach(d => {
-      const el = document.createElement("div");
-      el.className = "cal-day-name";
-      el.innerText = d;
-      grid.appendChild(el);
-    });
-
-    const firstDay = new Date(year, month, 1);
-    let start = firstDay.getDay();
-    if (start === 0) start = 7;
-
-    const daysInMonth = new Date(year, month+1, 0).getDate();
-
-    for (let i=1;i<start;i++){
-      grid.appendChild(document.createElement("div"));
-    }
-
-    for (let d=1; d<=daysInMonth; d++){
-      const date = new Date(year, month, d);
-      date.setHours(0,0,0,0);
-
-      const btn = document.createElement("button");
-      btn.className = "cal-day";
-      btn.innerText = d;
-
-      const dayOfWeek = date.getDay(); // 0 = воскресенье, 6 = суббота
-
-  if (dayOfWeek === 6) {
-    btn.classList.add("saturday");
-  }
-
-  if (dayOfWeek === 0) {
-    btn.classList.add("sunday");
-  }
-
-      if (date < today) {
-        btn.classList.add("disabled");
-        btn.disabled = true;
-      } else {
-        btn.onclick = () => {
-          document.querySelectorAll(".cal-day")
-            .forEach(x => x.classList.remove("selected"));
-          btn.classList.add("selected");
-          onSelect(date);
-        };
-      }
-
-      grid.appendChild(btn);
-    }
-
-    container.appendChild(grid);
-  }
 
   function nextMonth() {
     current.setMonth(current.getMonth() + 1);
