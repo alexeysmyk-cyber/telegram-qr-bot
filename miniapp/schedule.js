d8c9f6616f4d1e52c1a7f63e3a2aa1eba275ba9b
alexeysmyk-cyber <alexey.smyk@gmail.com>
2026-02-03 13:37:13 +0400

    Update schedule.js

--- miniapp/schedule.js
+++ miniapp/schedule.js
@@ -320,6 +320,12 @@ function isPast(dateString) {
 window.isLongPressActive = false;
 function attachSlotEvents() {
 
+  slot.addEventListener("touchcancel", () => {
+  clearTimeout(pressTimer);
+  deactivateLongPressMode(slot);
+  slot.style.transform = "";
+});
+
    console.log("attachSlotEvents called");
 
 
