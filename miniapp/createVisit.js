4c1e614123177cb1009cb1cd429655d2e92f04da
alexeysmyk-cyber <alexey.smyk@gmail.com>
2026-02-04 18:07:29 +0400

    Update createVisit.js

--- miniapp/createVisit.js
+++ miniapp/createVisit.js
@@ -25,7 +25,7 @@ if (fab) fab.style.display = "none";
         <div class="create-close" id="closeCreateBtn">✕</div>
       </div>
 
-      <div class="card" id="doctorContainer">
+      <div class="doctor-select-wrapper" id="createDoctorSelect">
         Загрузка врачей...
       </div>
 
